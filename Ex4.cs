19d24a43c35eb1e686442873a2f3a32b041783e1
Yurat13 <111081203+Yurat13@users.noreply.github.com>
2022-09-22 08:54:57 +0500

    Update Ex4.cs

--- Ex4.cs
+++ Ex4.cs
@@ -1,11 +1,41 @@
-﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
 
-int x = 1;
-int y = 1;
-int step = 0;
-    while (step < 10)
+int n = 10;
+int[,] matr = new int[n, n];
+const int cent = 3;
+
+void PascalTriangle()
+{
+    for (int i = 0; i < n; i++)
     {
-        double result = Math.Pow(x + y, step);
-        Console.WriteLine (result);
-        step++;
+        matr[i, 0] = 1;
+        matr[i, i] = 1;
     }
+    for (int i = 2; i < n; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            matr[i, j] = matr[i - 1, j - 1] + matr[i - 1, j];
+        }
+    }
+}
+
+void PrintArray()
+{
+    int shir = cent * n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.SetCursorPosition(shir, i + 1);
+            if (matr[i, j] != 0)
+                Console.Write($"{matr[i, j],cent}");
+            shir += cent*2;
+        }
+        shir = cent*n - cent * (i + 1);
+        Console.WriteLine();
+    }
+}
+
+PascalTriangle();
+PrintArray();
